dd096c2f14e15276b51dde8918d1b30b62d96ac5
Tim <kimtimmy93@gmail.com>
2024-09-17 23:32:15 -0400

    Add validation error modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,28 @@
+import { createPortal } from 'react-dom';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+
+const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open: () => {
+        dialog.current.showModal();
+      },
+      close: () => {
+        dialog.current.close();
+      }
+    }
+  })
+
+  return createPortal(
+    <dialog ref={dialog} className='backdrop:bg-stone-900/90 p-4 rounded-md shadow-md'>{children}
+      <form method="dialog">
+        <button>{buttonCaption}</button>
+      </form>
+    </dialog>,
+    document.getElementById('modal-root')
+  );
+})
+
+export default Modal;
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,39 +1,63 @@
 import React from 'react';
 import Input from './Input.jsx';
 import { useRef } from 'react';
-
+import Modal from './Modal.jsx';
 export default function NewProject({ onAdd }) {
-  const titleRef = useRef(null);
-  const descriptionRef = useRef(null);
-  const dueDateRef = useRef(null);
+  const modal = useRef();
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+  const dueDateRef = useRef();
 
   const handleSave = () => {
     const enteredTitle = titleRef.current.value;
     const enteredDescription = descriptionRef.current.value;
     const enteredDueDate = dueDateRef.current.value;
 
+    if (
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() === ''
+    ) {
+      modal.current.open();
+      return;
+    }
+
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
-      dueDate: enteredDueDate
+      dueDate: enteredDueDate,
     });
-  }
+  };
 
   return (
-    <div className='w-[35rem] mt-16'>
-      <menu className='flex items-center justify-end gap-4 my-4'>
-        <li>
-          <button className='text-stone-800 hover:text-stone-950'>Cancel</button>
-        </li>
-        <li>
-          <button className='bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md' onClick={handleSave}>Save</button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" label="Title" ref={titleRef} />
-        <Input label="Description" textarea ref={descriptionRef} />
-        <Input type="date" label="Due Date" ref={dueDateRef} />
+    <>
+      <Modal ref={modal} buttonCaption="OK">
+        <h2 className='text-xl font-bold text-stone-400 my-4'>Invalid Input</h2>
+        <p className='text-stone-400 mb-4'>Oops... looks like you forgot to enter a value</p>
+        <p className='text-stone-400 mb-4'>Please make sure you provide a valid value for every input field</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" label="Title" ref={titleRef} />
+          <Input label="Description" textarea ref={descriptionRef} />
+          <Input type="date" label="Due Date" ref={dueDateRef} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
