5915b22d786fa40bbed929338964ee16cf629579
Tim <kimtimmy93@gmail.com>
2024-09-19 16:47:01 -0400

    add cancel button logic

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Input from './Input.jsx';
 import { useRef } from 'react';
 import Modal from './Modal.jsx';
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modal = useRef();
   const titleRef = useRef();
   const descriptionRef = useRef();
@@ -33,13 +33,13 @@ export default function NewProject({ onAdd }) {
     <>
       <Modal ref={modal} buttonCaption="OK">
         <h2 className='text-xl font-bold text-stone-400 my-4'>Invalid Input</h2>
-        <p className='text-stone-400 mb-4'>Oops... looks like you forgot to enter a value</p>
-        <p className='text-stone-400 mb-4'>Please make sure you provide a valid value for every input field</p>
+        <p className='text-stone-600 mb-4'>Oops... looks like you forgot to enter a value</p>
+        <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field</p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
               Cancel
             </button>
           </li>
